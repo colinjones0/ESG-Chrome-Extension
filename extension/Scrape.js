4ecbda0d36eaad35299b63b7fa28b99278565540
Claire Oberg <claire@obergonline.com>
2021-04-16 15:52:08 -0400

    added graphics for loading

--- extension/Scrape.js
+++ extension/Scrape.js
@@ -1,4 +1,5 @@
-document.getElementById("score").addEventListener("click", Scrape);
+//document.getElementById("score").addEventListener("click", Scrape);
+document.addEventListener("load", Scrape);
 
 function myFunction() {
     console.log('hi');
@@ -40,6 +41,18 @@ function Scrape() {
         )
             .then((response) => {
                 console.log(response.data["recommendations"])
+                console.log(response.data["recommendations"][3][4])
+                document.getElementById("score").innerHTML = response.data["recommendations"][3][4];
+                document.getElementById("recommendation-1-website").innerHTML = response.data["recommendations"][0][0];
+                document.getElementById("recommendation-1-website").href = response.data["recommendations"][0][2];
+                document.getElementById("recommendation-2-website").innerHTML = response.data["recommendations"][1][0];
+                document.getElementById("recommendation-2-website").href = response.data["recommendations"][1][2];
+                document.getElementById("recommendation-3-website").innerHTML = response.data["recommendations"][2][0];
+                document.getElementById("recommendation-3-website").href = response.data["recommendations"][2][2];
+                document.getElementById("environment-article").href = response.data["recommendations"][3][5];
+                document.getElementById("social-article").href = response.data["recommendations"][3][6];
+                document.getElementById("governance-article").href = response.data["recommendations"][3][7];
+
                 return response.data["recommendations"]
             });
     }
